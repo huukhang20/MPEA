db9a8a1ba9ee9b3bfb93e2b762beb92860ecae88
huukhang20 <huukhanggl02@gmail.com>
2024-10-02 18:35:53 +0700

    add mapper

---
+++ MPEA.Application/IService/IUserSerevice.cs
@@ -0,0 +1,14 @@
+﻿using MPEA.Application.Model.ViewModel.User;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MPEA.Application.IService
+{
+    public interface IUserSerevice
+    {
+        Task<List<UserResponse>> GetAllAccount();
+    }
+}
---
+++ MPEA.Application/Service/UserService.cs
@@ -0,0 +1,27 @@
+﻿using MPEA.Application.IService;
+using MPEA.Application.Model.ViewModel.User;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MPEA.Application.Service
+{
+    public class UserService : IUserSerevice
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public UserService(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<List<UserResponse>> GetAllAccount()
+        {
+            var userList = await _unitOfWork.UserRepository.GetAllAsync();
+            var result = new List<UserResponse>();
+            return result;
+        }
+    }
+}
