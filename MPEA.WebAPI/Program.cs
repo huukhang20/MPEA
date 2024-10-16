539a510e53ff6f43f23a36a9835386927ac337da
huukhang20 <huukhanggl02@gmail.com>
2024-10-16 12:12:06 +0700

    update

--- MPEA.WebAPI/Program.cs
+++ MPEA.WebAPI/Program.cs
@@ -15,17 +15,17 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+//if (app.Environment.IsDevelopment())
+//{
+//    app.UseSwagger();
+//    app.UseSwaggerUI();
+//}
 
-app.UseDefaultFiles();
+//app.UseDefaultFiles();
 
-app.UseStaticFiles();
+//app.UseStaticFiles();
 
-app.UseRouting();
+//app.UseRouting();
 
 app.UseHttpsRedirection();
 
