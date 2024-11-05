315f25833cae5bfacb7c7c000819f6b58121e0f1
huukhang20 <huukhanggl02@gmail.com>
2024-11-05 13:27:42 +0700

    update

--- MPEA.Application/Model/ViewModel/User/UserResponse.cs
+++ MPEA.Application/Model/ViewModel/User/UserResponse.cs
@@ -8,6 +8,7 @@ namespace MPEA.Application.Model.ViewModel.User
 {
     public class UserResponse
     {
+        public Guid? Id { get; set; }
         public string? UserName { get; set; }
         public string? FullName { get; set; }
         public string? Password { get; set; }
